a45f23917b6ffc310575b4bb4c5ca5a19509d3c7
domcaio <141692273+domcaio@users.noreply.github.com>
2023-11-20 14:56:36 -0300

    Add files via upload

---
+++ Atv01.java
@@ -0,0 +1,6 @@
+public class Atv01 {
+    public static void main(String[] args) {
+        int x = 2, y = 3, z = 4; // aqui estamos declarando tres variaveis na mesma linha com o metodo comma e atribuindo valores a elas
+        System.out.println(x + y / z); // aqui estamos pedindo para imprimir no console o resultado da operação utilizando as variaveis, quais possuem os valores declarados
+    }
+}
